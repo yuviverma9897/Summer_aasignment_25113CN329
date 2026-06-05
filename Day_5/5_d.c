6fbae10cf1d7c53d1d2929ac26fa15b1bbbd4c0e
Yuvraj Verma <yverma9897@gmail.com>
2026-06-05 11:38:29 +0530

    Day_5 is uploaded.

--- Day_5/5_d.c
+++ Day_5/5_d.c
@@ -1,29 +1,37 @@
-//to print largest prime factor
-#include<stdio.h>
-int main(){
-    int num, la_pr_fct=0;
+// to print largest prime factor
+#include <stdio.h>
+int main()
+{
+    int num, la_pr_fct = 0;
     printf("Enter a number\n");
     scanf("%d", &num);
 
-    for(int i = 2; i <= num; i++) {
-        if(num%i == 0) {
+    for (int i = 2; i <= num; i++)
+    {
+        if (num % i == 0)
+        {
             int prime = 1;
-            for(int j = 2; j <= i/2; j++) {
-                if(i % j == 0) {
+            for (int j = 2; j <= i / 2; j++)
+            {
+                if (i % j == 0)
+                {
                     prime = 0;
                     break;
                 }
             }
-            if(prime){
+            if (prime)
+            {
                 la_pr_fct = i;
             }
         }
     }
 
-    if (la_pr_fct != 0){
+    if (la_pr_fct != 0)
+    {
         printf("Its largest prime factor is %d", la_pr_fct);
     }
-    else{
+    else
+    {
         printf("No prime factors exists");
     }
     return 0;
